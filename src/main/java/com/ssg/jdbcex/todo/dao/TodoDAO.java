b8698373124ec95fb0d7a09be2ad9a95c9aef755
KangChangSeon <rlckdtjs@naver.com>
2025-04-08 14:07:31 +0900

    25-04-08

--- src/main/java/com/ssg/jdbcex/todo/dao/TodoDAO.java
+++ src/main/java/com/ssg/jdbcex/todo/dao/TodoDAO.java
@@ -1,11 +1,9 @@
 package com.ssg.jdbcex.todo.dao;
 
+import com.ssg.jdbcex.todo.domain.TodoVO;
 import lombok.Cleanup;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class TodoDAO {
     // DB 로부터 시간 얻어오는 간단한 기능 구현
@@ -31,5 +29,19 @@ public class TodoDAO {
         rs.next();
         String now = rs.getString(1);
         return now;
+    }
+
+    // tbl_todo 테이블에 todo 를 넣는 Insert 기능(TodoVO vo)
+    public void insert(TodoVO vo) throws SQLException {
+        // 쿼리문 생성
+        String sql = "INSERT INTO tbl_todo(title,duedate,finished) VALUES(?,?,?)";
+        @Cleanup Connection conn = ConnectionUtil.INSTANCE.getConnection();
+        @Cleanup PreparedStatement ps = conn.prepareStatement(sql);
+
+        ps.setString(1, vo.getTitle());
+        ps.setDate(2, Date.valueOf(vo.getDuedate().toLocalDate()));
+        ps.setBoolean(3, vo.isFinished());
+
+        ps.executeUpdate();
     }
 }
--- src/test/java/com/ssg/dao/TodoDAOTests.java
+++ src/test/java/com/ssg/dao/TodoDAOTests.java
@@ -1,8 +1,11 @@
 package com.ssg.dao;
 
 import com.ssg.jdbcex.todo.dao.TodoDAO;
+import com.ssg.jdbcex.todo.domain.TodoVO;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
 
 // 단위 테스트
 public class TodoDAOTests {
@@ -21,5 +24,15 @@ public class TodoDAOTests {
     @Test
     public void testTime1() throws Exception {
         System.out.println(todoDAO.getTime2());
+    }
+
+    @Test
+    public void testInsert() throws Exception {
+        TodoVO vo = TodoVO.builder()
+                .title("다섯")
+                .duedate(LocalDateTime.now())
+                .finished(false)
+                .build();
+        todoDAO.insert(vo);
     }
 }
